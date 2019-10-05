eb6545541ee4f0c3c76a28f7999408e11de9aacd
Pieter Muller <mullerp@amazon.com>
2019-10-05 23:32:04 +0200

    Add hacked up email reading proof of concept.
    Does not yet work with Secure POP3, only POP3.

--- mail_check.py
+++ mail_check.py
@@ -1,4 +1,6 @@
 import os
+import poplib
+import email
 
 def get_config(path):
     config = {}
@@ -14,11 +16,39 @@ def get_config(path):
         config[key] = value
     return config
 
+def list_inbox(config):
+    server = config['server']
+    port = config['port']
+    username = config['username']
+    password = config['password']
+    print(f"Server {server}, Port {port}")
+    popclient = poplib.POP3(server, port)
+    popclient.user(username)
+    popclient.pass_(password)
+    print("Connected, I think")
+    print(popclient.stat())
+
+    msg = popclient.retr(2)
+    raw_email = b"\n".join(msg[1])
+    parsed_email = email.message_from_bytes(raw_email)
+    print(parsed_email)
+
+    # get multiple parts from message body.
+    parts = parsed_email.get_payload()
+    # loop for each part
+    for n, part in enumerate(parts):
+        # print multiple part information by invoke print_info function recursively.
+        content_type = part.get_content_type() 
+        print(f"Part {n}: type: {content_type}")
+        print(part.get_payload(decode=True))
 
 home = os.getenv("HOME")
 config_path = home +'/.mail_to_ynab'
 
 print("Hello world!")
 
-print(get_config(config_path))
+config = get_config(config_path)
+print("Config:")
+print(config)
 
+list_inbox(config)
