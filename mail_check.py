1f47e90c3cce7f462f953547ebbfe007aa40f833
Pieter Muller <mullerp@amazon.com>
2019-10-13 16:20:51 +0200

    Move orchestration out of mail_check into a new class.

--- mail_check.py
+++ mail_check.py
@@ -1,81 +1,40 @@
-import os
 import poplib
 import email
-from discovery_za_parser import DiscoveryZaParser
-from ynab_client import YnabClient
 
-def get_config(path):
-    config = {}
-    f= open(path,'r')
-    lines = f.readlines()
-    for l in lines:
-        tokens = l.split(':')
-        if len(tokens) != 2:
-            raise "Expected config lines to contain exactly two fields"
-        key = tokens[0].strip()
-        value = tokens[1].strip()
-        config[key] = value
-    return config
+class MailChecker:
 
-def get_pop_client(config):
-    server = config['server']
-    port = config['port']
-    username = config['username']
-    password = config['password']
-    print(f"Server {server}, Port {port}")
-    popclient = poplib.POP3(server, port)
-    popclient.user(username)
-    popclient.pass_(password)
-    return popclient
+    def __init__(self, server, port, username, password):
+        self.inbox = self.get_pop_client(server, port, username, password)
+        print("Connected, I think")
 
-# Returns a list of parsed emails
-def list_inbox(inbox):
-    msg_count, inbox_size = inbox.stat()
-    print(f"Found {msg_count} messages")
-    if msg_count < 1:
-        return []
-    msgs = []
-    for i in range(1,msg_count+1):
-        print(f"Fetching message {i}")
-        msg = inbox.retr(i)
-        raw_email = b"\n".join(msg[1])
-        parsed_email = email.message_from_bytes(raw_email)
-        msgs.append(parsed_email)
-    return msgs
+    def get_pop_client(self, server, port, username, password):
+        print(f"Server {server}, Port {port}")
+        popclient = poplib.POP3(server, port)
+        popclient.user(username)
+        popclient.pass_(password)
+        return popclient
 
-def extract_text(parsed_email):
-    for part in parsed_email.walk():
-        type = part.get_content_type()
-        # Right now, just returns the first text part, which works fine for the current email format
-        if type == "text/plain":
-            text = part.get_payload(decode=True)
-            print("\nFound plain text:")
-            print(text)
-            return text
+    # Returns a list of parsed emails
+    def list_inbox(self):
+        msg_count, inbox_size = self.inbox.stat()
+        print(f"Found {msg_count} messages")
+        if msg_count < 1:
+            return []
+        msgs = []
+        for i in range(1,msg_count+1):
+            print(f"Fetching message {i}")
+            msg = self.inbox.retr(i)
+            raw_email = b"\n".join(msg[1])
+            parsed_email = email.message_from_bytes(raw_email)
+            msgs.append(parsed_email)
+        return msgs
 
-home = os.getenv("HOME")
-config_path = home +'/.mail_to_ynab'
-
-config = get_config(config_path)
-print(f"Config: {config}")
-
-api_key = config['ynab_api_key']
-budget_id = config['ynab_budget']
-account_id = config['ynab_account']
-
-
-ynab = YnabClient(api_key, budget_id, account_id)
-ynab.test_ynab()
-
-inbox = get_pop_client(config)
-print("Connected, I think")
-msgs = list_inbox(inbox)
-
-parser = DiscoveryZaParser()
-for msg in msgs:
-    text = extract_text(msg)
-    if parser.looks_like_notification(text):
-        print("Looks like a notification")
-        parser.get_transaction(text)
-    else:
-        print("Not what we are looking for")
+    def extract_text(self, parsed_email):
+        for part in parsed_email.walk():
+            type = part.get_content_type()
+            # Right now, just returns the first text part, which works fine for the current email format
+            if type == "text/plain":
+                text = part.get_payload(decode=True)
+                print("\nFound plain text:")
+                print(text)
+                return text
---
+++ mail_to_ynab.py
@@ -0,0 +1,55 @@
+import os
+from ynab_client import YnabClient
+from mail_check import MailChecker
+from discovery_za_parser import DiscoveryZaParser
+
+class MailToYnab:
+
+    def __init__(self, config_path):
+        self.config = self.get_config(config_path)
+        print(f"Config: {self.config}")
+
+        api_key = self.config['ynab_api_key']
+        budget_id = self.config['ynab_budget']
+        account_id = self.config['ynab_account']
+        self.ynab = YnabClient(api_key, budget_id, account_id)
+
+        server = self.config['server']
+        port = self.config['port']
+        username = self.config['username']
+        password = self.config['password']
+        self.mail = MailChecker(server, port, username, password)
+
+        self.parser = DiscoveryZaParser()
+
+    def run(self):
+        self.ynab.test_ynab()
+
+        msgs = self.mail.list_inbox()
+
+        for msg in msgs:
+            text = self.mail.extract_text(msg)
+            if self.parser.looks_like_notification(text):
+                print("Looks like a notification")
+                self.parser.get_transaction(text)
+            else:
+                print("Not what we are looking for")
+
+    def get_config(self, path):
+        config = {}
+        f= open(path,'r')
+        lines = f.readlines()
+        for l in lines:
+            tokens = l.split(':')
+            if len(tokens) != 2:
+                raise "Expected config lines to contain exactly two fields"
+            key = tokens[0].strip()
+            value = tokens[1].strip()
+            config[key] = value
+        return config
+
+if __name__ == "__main__":
+    home = os.getenv("HOME")
+    config_path = home +'/.mail_to_ynab'
+    mty = MailToYnab(config_path)
+    mty.run()
