2446105546100e8267f2e3021622a71035542a29
Pieter Muller <pfdevmuller@gmail.com>
2019-10-18 12:58:22 +0200

    Make mail connection lazy.

--- mail_check.py
+++ mail_check.py
@@ -4,8 +4,17 @@ import email
 class MailChecker:
 
     def __init__(self, server, port, username, password):
-        self.inbox = self.get_pop_client(server, port, username, password)
-        print("Connected, I think")
+        self.server = server
+        self.port = port
+        self.username = username
+        self.password = password
+        self.inbox = None
+
+    def get_inbox(self):
+        if (self.inbox is None):
+            self.inbox = self.get_pop_client(self.server, self.port, self.username, self.password)
+            print("Connected, I think")
+        return self.inbox
 
     def get_pop_client(self, server, port, username, password):
         print(f"Server {server}, Port {port}")
@@ -16,14 +25,14 @@ class MailChecker:
 
     # Returns a list of parsed emails
     def list_inbox(self):
-        msg_count, inbox_size = self.inbox.stat()
+        msg_count, inbox_size = self.get_inbox().stat()
         print(f"Found {msg_count} messages")
         if msg_count < 1:
             return []
         msgs = []
         for i in range(1,msg_count+1):
             print(f"Fetching message {i}")
-            msg = self.inbox.retr(i)
+            msg = self.get_inbox().retr(i)
             raw_email = b"\n".join(msg[1])
             parsed_email = email.message_from_bytes(raw_email)
             msgs.append(parsed_email)
