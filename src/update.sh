f3baee4e6a521ae43f4db1d1e5c5a78b5b47ad27
Pieter Muller <pfdevmuller@gmail.com>
2019-12-16 22:37:32 +0200

    Cleanup

--- src/update.sh
+++
@@ -1,4 +0,0 @@
-zip mailtoynab_lambda.zip mail_to_ynab.py
-aws lambda update-function-code --function-name MailToYnab --zip-file fileb://mailtoynab_lambda.zip
-aws lambda update-function-configuration --function-name MailToYnab --handler mail_to_ynab.lambda_handler
-aws lambda invoke --function-name MailToYnab --log-type Tail /tmp/lambda_out |jq -r '.LogResult' | base64 --decode
