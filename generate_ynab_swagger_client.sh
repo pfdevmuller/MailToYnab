bca9af48349cb4429f7d61b9099f901d08710d43
Pieter Müller <pfdevmuller@gmail.com>
2020-04-27 15:13:46 +0200

    Add comment about which swagger-codegen to use.

--- generate_ynab_swagger_client.sh
+++ generate_ynab_swagger_client.sh
@@ -1,1 +1,2 @@
+# The below uses swagger-codegen version 3.0.18, which can be acquired on OSX using `brew install swagger-codegen`
 swagger-codegen generate -i https://api.youneedabudget.com/papi/spec-v1-swagger.json -l python -o ./dependencies/ynab_v1
