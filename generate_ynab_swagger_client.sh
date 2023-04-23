f5950723594d8d6f7af425138b0ea98af91e5a96
Pieter Muller <mullerp@amazon.com>
2023-04-23 17:03:45 +0200

    Correct swagger script target folder

--- generate_ynab_swagger_client.sh
+++ generate_ynab_swagger_client.sh
@@ -1,2 +1,2 @@
 # The below uses swagger-codegen version 3.0.18, which can be acquired on OSX using `brew install swagger-codegen`
-swagger-codegen generate -i https://api.youneedabudget.com/papi/spec-v1-swagger.json -l python -o ./dependencies/ynab_v1
+swagger-codegen generate -i https://api.youneedabudget.com/papi/spec-v1-swagger.json -l python -o ./dependencies/ynab-v1
